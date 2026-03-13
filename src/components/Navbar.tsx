80a4a33f7a324b36b6c7f5660011161731849f43
Jane Batakariza <janebatakariza@gmail.com>
2026-03-13 12:12:03 +0200

    designed navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,10 +3,15 @@ import { Link } from "react-router-dom";
 export default function NavBar() {
   return (
     <nav id="navBar">
+        <div id="navLinks">
       <Link to="/">Home</Link>
       <Link to="/marketplace">MarketPlace</Link>
       <Link to="/dashboard">Dashboard</Link>
       <Link to="/traceability">Traceability</Link>
+   </div>
+   <div id="navRight">
+    <button id="connectBtn">Connect Wallet</button>
+   </div>
     </nav>
   );
 }
