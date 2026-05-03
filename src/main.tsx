2b72072f166c7758fb5335019eee90350af056fe
Jane Batakariza <janebatakariza@gmail.com>
2026-05-03 10:27:23 +0200

    background of the  landing page

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import Agri from "./LandingPage.tsx";
+import Agri from "./pages/LandingPage";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./LandingPage.css";
+import './Landing.css';
 function LandingPage() {
   return (
     <>
