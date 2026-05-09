44cb4e5c075ba53569efa26e73b934ef7d08a1c7
Jane Batakariza <janebatakariza@gmail.com>
2026-05-09 21:32:51 +0200

    styled registration part

--- src/pages/registration.tsx
+++ src/pages/registration.tsx
@@ -1,6 +1,19 @@
+import './Registration.css';
 function Registration(){
 return (
     <>
+    <div className="role">
+<h1 className="register">Register to Agrihash</h1>
+<p className="first_two">Farming enterprise</p>
+<p className="first_two">Agro-Processing</p>
+<p className="second_two">Warehouse</p>
+<p className="second_two">Transport Company</p>
+<p className="third_two">Inspector</p>
+<p className="third_two">Auditor</p>
+<p className="fourth_two">Retailer</p>
+<button>Continue</button>
+
+    </div>
     </>
 )
 }
